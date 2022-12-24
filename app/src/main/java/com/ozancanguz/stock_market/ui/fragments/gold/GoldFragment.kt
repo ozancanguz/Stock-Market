1aa4b7aac69fa7e9892ef7b8679d9142aa9f46a7
Ozan <ozancangu24@gmail.com>
2022-12-25 00:14:41 +0300

    -data displayed in logcat

--- app/src/main/java/com/ozancanguz/stock_market/ui/fragments/gold/GoldFragment.kt
+++ app/src/main/java/com/ozancanguz/stock_market/ui/fragments/gold/GoldFragment.kt
@@ -1,9 +1,12 @@
 package com.ozancanguz.stock_market.ui.fragments.gold
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.FirebaseApp
@@ -12,6 +15,7 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.ozancanguz.stock_market.R
 import com.ozancanguz.stock_market.databinding.FragmentGoldBinding
+import com.ozancanguz.stock_market.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +24,7 @@ class GoldFragment : Fragment() {
 
     private val binding get() = _binding!!
     private lateinit var auth: FirebaseAuth
+    private val mainViewModel:MainViewModel by viewModels()
 
 
     override fun onCreateView(
@@ -34,14 +39,28 @@ class GoldFragment : Fragment() {
         auth = Firebase.auth
 
         setHasOptionsMenu(true)
+
+        observeLiveData()
+
         return view
     }
 
+    private fun observeLiveData() {
+        mainViewModel.requestGoldPrices()
+        mainViewModel.goldPriceList.observe(viewLifecycleOwner, Observer {
+            Log.d("fragment",""+it)
+        })
+    }
+
+
+    // create menu
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.signoutmenu,menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
+
+    // menu onclick listener
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if(item.itemId==R.id.signOutMenuu){
             auth.signOut()
