70fe8fab6be33e2947051a6c9ed7cb297fe7d782
Ozan <ozancangu24@gmail.com>
2022-12-24 22:44:53 +0300

    -sign out implemented

--- app/src/main/java/com/ozancanguz/stock_market/MainActivity.kt
+++ app/src/main/java/com/ozancanguz/stock_market/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ozancanguz.stock_market
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 
@@ -9,16 +11,24 @@ import androidx.navigation.ui.AppBarConfiguration
 
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
         navController = findNavController(R.id.navHostFragment)
         val appBarConfiguration = AppBarConfiguration(
@@ -39,4 +49,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
     }
+
+
 }
--- app/src/main/java/com/ozancanguz/stock_market/ui/fragments/gold/GoldFragment.kt
+++ app/src/main/java/com/ozancanguz/stock_market/ui/fragments/gold/GoldFragment.kt
@@ -1,10 +1,15 @@
 package com.ozancanguz.stock_market.ui.fragments.gold
 
 import android.os.Bundle
+import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ozancanguz.stock_market.R
 import com.ozancanguz.stock_market.databinding.FragmentGoldBinding
 
@@ -13,6 +18,8 @@ class GoldFragment : Fragment() {
     private var _binding: FragmentGoldBinding? = null
 
     private val binding get() = _binding!!
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,8 +28,27 @@ class GoldFragment : Fragment() {
         _binding = FragmentGoldBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        FirebaseApp.initializeApp(requireContext())
+        // Initialize Firebase Auth
+        auth = Firebase.auth
 
+        setHasOptionsMenu(true)
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.signoutmenu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.signOutMenuu){
+            auth.signOut()
+            findNavController().navigate(R.id.action_goldFragment_to_loginActivity)
+            Toast.makeText(requireContext(),"Succesfully signout",Toast.LENGTH_LONG).show()
+            
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
