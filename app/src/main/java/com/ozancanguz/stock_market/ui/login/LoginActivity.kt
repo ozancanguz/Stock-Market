21c5ab26a6dc3baa6b7ee7f32355c5127975c925
Ozan <ozancangu24@gmail.com>
2022-12-24 22:25:01 +0300

    -user register completed

--- app/src/main/java/com/ozancanguz/stock_market/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ozancanguz/stock_market/ui/login/LoginActivity.kt
@@ -3,12 +3,22 @@ package com.ozancanguz.stock_market.ui.login
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.ozancanguz.stock_market.MainActivity
 import com.ozancanguz.stock_market.R
 import com.ozancanguz.stock_market.databinding.ActivityLoginBinding
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,10 +26,42 @@ class LoginActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        binding.registerBtn.setOnClickListener {
-            val intent= Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        }
+        FirebaseApp.initializeApp(this)
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+             register()
+
+    }
+
+
+        private fun register() {
+
+            binding.registerBtn.setOnClickListener {
+                val email = binding.emailET.text.toString()
+                val password = binding.passwordET.text.toString()
+
+                if (email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(this, "E-mail or password empty", Toast.LENGTH_LONG).show()
+                } else {
+
+                    auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+
+                        Snackbar.make(
+                            loginlayout,
+                            "User Created.Click on sign in button",
+                            Snackbar.LENGTH_LONG
+                        )
+                            .show()
+                    }.addOnFailureListener {
+                        Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG)
+                            .show()
+                    }
+
+
+                }
+            }
+
 
     }
 }
