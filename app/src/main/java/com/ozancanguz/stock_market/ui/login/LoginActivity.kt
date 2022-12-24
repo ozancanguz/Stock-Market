41553cd37b4d3559b39eb79089376bc811008a00
Ozan <ozancangu24@gmail.com>
2022-12-24 22:29:28 +0300

    -user sign in completed

--- app/src/main/java/com/ozancanguz/stock_market/ui/login/LoginActivity.kt
+++ app/src/main/java/com/ozancanguz/stock_market/ui/login/LoginActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
@@ -30,7 +31,11 @@ class LoginActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         auth = Firebase.auth
 
+           // user register
              register()
+
+        // user sign in
+              signIn()
 
     }
 
@@ -62,6 +67,26 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
 
+    }
+    private fun signIn() {
+        binding.signInBtn.setOnClickListener {
+            val email = binding.emailET.text.toString()
+            val password = binding.passwordET.text.toString()
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "E-mail or password empty", Toast.LENGTH_LONG)
+                    .show()
+            } else {
 
+                auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+                    val intent=Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+
+                }.addOnFailureListener {
+                    Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+                }
+
+
+            }
+        }
     }
 }
