71a650b4c647189118b70d359937c95d4974d8bc
Ozan <ozancangu24@gmail.com>
2022-12-25 16:25:32 +0300

    -data displayed in logcat

--- app/src/main/java/com/ozancanguz/stock_market/ui/fragments/parity/ParityFragment.kt
+++ app/src/main/java/com/ozancanguz/stock_market/ui/fragments/parity/ParityFragment.kt
@@ -1,12 +1,16 @@
 package com.ozancanguz.stock_market.ui.fragments.parity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.ozancanguz.stock_market.R
 import com.ozancanguz.stock_market.databinding.FragmentParityBinding
+import com.ozancanguz.stock_market.viewmodels.ParityViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +18,9 @@ class ParityFragment : Fragment() {
     private var _binding: FragmentParityBinding? = null
 
     private val binding get() = _binding!!
+
+    private val parityViewModel:ParityViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +30,15 @@ class ParityFragment : Fragment() {
         val view = binding.root
 
 
+        observeLiveData()
         return view
+    }
+
+    private fun observeLiveData() {
+        parityViewModel.requestParityData()
+        parityViewModel.parityList.observe(viewLifecycleOwner, Observer {
+            Log.d("parityfragment",""+it)
+        })
     }
 
 
