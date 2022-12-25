2d8face503405b6ff60e2841a34d32069b821b38
Ozan <ozancangu24@gmail.com>
2022-12-25 17:54:03 +0300

    -ware model classes are created

---
+++ app/src/main/java/com/ozancanguz/stock_market/data/model/ware/Result.kt
@@ -0,0 +1,27 @@
+package com.ozancanguz.stock_market.data.model.ware
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Result(
+    @SerializedName("buying")
+    val buying: Double,
+    @SerializedName("buyingstr")
+    val buyingstr: String,
+    @SerializedName("date")
+    val date: String,
+    @SerializedName("datetime")
+    val datetime: String,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("rate")
+    val rate: Any,
+    @SerializedName("selling")
+    val selling: Double,
+    @SerializedName("sellingstr")
+    val sellingstr: String,
+    @SerializedName("text")
+    val text: String,
+    @SerializedName("time")
+    val time: String
+)
