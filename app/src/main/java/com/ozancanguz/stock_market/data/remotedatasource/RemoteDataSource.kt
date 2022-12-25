2274230d04613bcac47d570438787c35f557f723
Ozan <ozancangu24@gmail.com>
2022-12-25 13:11:41 +0300

    -remote data source modified

--- app/src/main/java/com/ozancanguz/stock_market/data/remotedatasource/RemoteDataSource.kt
+++ app/src/main/java/com/ozancanguz/stock_market/data/remotedatasource/RemoteDataSource.kt
@@ -3,6 +3,7 @@ package com.ozancanguz.stock_market.data.remotedatasource
 import com.ozancanguz.stock_market.data.api.StockApi
 import com.ozancanguz.stock_market.data.model.crpto.Crypto
 import com.ozancanguz.stock_market.data.model.gold.Gold
+import com.ozancanguz.stock_market.data.model.stock.Stock
 import retrofit2.Response
 import javax.inject.Inject
 
@@ -16,6 +17,10 @@ class RemoteDataSource@Inject constructor(private val stockApi: StockApi) {
 
     suspend fun getCryptoPrices():Response<Crypto>{
         return stockApi.getCryptoPrices()
+    }
+
+    suspend fun getStockPrices():Response<Stock>{
+        return stockApi.getStockPrices()
     }
 
 
