85c5f497d3b44388ee5f938892374468821126e6
Ozan <ozancangu24@gmail.com>
2022-12-25 13:17:46 +0300

    -stock data displayed in logcat

--- app/src/main/java/com/ozancanguz/stock_market/ui/fragments/stock/StockFragment.kt
+++ app/src/main/java/com/ozancanguz/stock_market/ui/fragments/stock/StockFragment.kt
@@ -1,12 +1,16 @@
 package com.ozancanguz.stock_market.ui.fragments.stock
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import com.ozancanguz.stock_market.R
 import com.ozancanguz.stock_market.databinding.FragmentStockBinding
+import com.ozancanguz.stock_market.viewmodels.StockViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +18,8 @@ class StockFragment : Fragment() {
     private var _binding: FragmentStockBinding? = null
 
     private val binding get() = _binding!!
+
+    private val stockViewModel:StockViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +29,20 @@ class StockFragment : Fragment() {
         val view = binding.root
 
 
+        // observe live data
+        observeLiveData()
+
+
 
 
         return view
+    }
+
+    private fun observeLiveData() {
+        stockViewModel.getStockPrices()
+        stockViewModel.stockList.observe(viewLifecycleOwner, Observer {
+            Log.d("stockfragment",""+it)
+        })
     }
 
 
