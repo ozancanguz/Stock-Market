12bf9db780e3844f27b67b8395a0c095b84f0fe7
Ozan <ozancangu24@gmail.com>
2022-12-25 10:50:02 +0300

    -crypto adapter and rv row layout created

---
+++ app/src/main/java/com/ozancanguz/stock_market/data/adapters/CrytpoAdapter.kt
@@ -0,0 +1,43 @@
+package com.ozancanguz.stock_market.data.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ozancanguz.stock_market.R
+import com.ozancanguz.stock_market.data.model.crpto.Crypto
+import com.ozancanguz.stock_market.data.model.crpto.Result
+import kotlinx.android.synthetic.main.crypto_row_layout.view.*
+import java.util.concurrent.CyclicBarrier
+
+class CrytpoAdapter:RecyclerView.Adapter<CrytpoAdapter.CrytoViewHolder>() {
+
+    var cryptoList= emptyList<Result>()
+
+    fun setData(newData: Crypto){
+        this.cryptoList=newData.result
+        notifyDataSetChanged()
+    }
+    class CrytoViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrytoViewHolder {
+        val inflater=LayoutInflater.from(parent.context)
+        val view=inflater.inflate(R.layout.crypto_row_layout,parent,false)
+        return CrytoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CrytoViewHolder, position: Int) {
+        val currentCrypto=cryptoList[position]
+        holder.itemView.crypto_img.setImageResource(R.drawable.crypto)
+        holder.itemView.crypto_name.text=currentCrypto.name
+        holder.itemView.crypto_price.text=currentCrypto.price.toString()+" USD"
+    }
+
+    override fun getItemCount(): Int {
+        return cryptoList.size
+    }
+
+
+}
